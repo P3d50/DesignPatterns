ae2b7e7c4b7e048bec56b69a92dafc4b26be3616
P3d50 <rpedromarcos@gmail.com>
2020-12-18 12:14:37 -0200

    builder

---
+++ src/builder/BBBoletoBuilder.java
@@ -0,0 +1,42 @@
+package builder;
+
+public class BBBoletoBuilder implements BoletoBuilder{
+
+	private String sacado;
+	private String cedente;
+	private double valor;
+	private String vencimento;
+	
+	@Override
+	public Boleto getBoleto() {
+ 		return new BBBoleto(sacado,cedente,valor,vencimento);
+	}
+
+	@Override
+	public void buildSacado(String sacado) {
+		
+		this.sacado = sacado;
+	}
+
+	@Override
+	public void buildCedente(String cedente) {
+		this.cedente= cedente;
+		
+	}
+
+	@Override
+	public void buildValor(double valor) {
+		this.valor=valor;
+		
+	}
+
+	@Override
+	public void buildVencimento(String vencimento) {
+		this.vencimento=vencimento;
+		
+	}
+
+	
+	
+
+}
